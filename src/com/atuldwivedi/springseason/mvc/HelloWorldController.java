2056777a7a46222236cb4c5761ac7fd4f7c01fbb
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-09-18 23:43:35 +0530

    reverted back to dynamic web project

--- src/com/atuldwivedi/springseason/mvc/HelloWorldController.java
+++ src/com/atuldwivedi/springseason/mvc/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.atuldwivedi.springseason;
+package com.atuldwivedi.springseason.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
